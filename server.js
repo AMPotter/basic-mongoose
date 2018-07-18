470bb77708a437c815132d2b5176f759578ef275
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2018-07-18 11:39:58 -0700

    add server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+/* eslint no-console: off */
+require('dotenv').config();
+const { createServer } = require('http');
+const connect = require('./lib/connect');
+connect('mongodb://localhost:27017/guitars');
+const app = require('./lib/app');
+
+const PORT = process.env.PORT || 3000;
+const server = createServer(app);
+
+server.listen(PORT, () => {
+    console.log('server running on port:', server.address().port);
+});
